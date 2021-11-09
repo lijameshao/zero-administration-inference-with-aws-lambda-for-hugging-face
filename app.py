ce6e28133b0ef4997c922b0c74f2d81f360abb95
James Li <lijameshao@yahoo.co.uk>
2021-11-09 07:14:04 +0000

    added api gateway

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from aws_cdk import (
     aws_apigateway as api_gw,
     aws_efs as efs,
     aws_ec2 as ec2,
-    core as cdk
+    core as cdk,
 )
 
 
@@ -19,48 +19,61 @@ class ServerlessHuggingFaceStack(cdk.Stack):
         super().__init__(scope, id, **kwargs)
 
         # EFS needs to be setup in a VPC
-        vpc = ec2.Vpc(self, 'Vpc', max_azs=2)
+        vpc = ec2.Vpc(self, "Vpc", max_azs=2)
 
         # creates a file system in EFS to store cache models
-        fs = efs.FileSystem(self, 'FileSystem',
-                            vpc=vpc,
-                            removal_policy=cdk.RemovalPolicy.DESTROY)
-        access_point = fs.add_access_point('MLAccessPoint',
-                                           create_acl=efs.Acl(
-                                               owner_gid='1001', owner_uid='1001', permissions='750'),
-                                           path="/export/models",
-                                           posix_user=efs.PosixUser(gid="1001", uid="1001"))
+        fs = efs.FileSystem(
+            self, "FileSystem", vpc=vpc, removal_policy=cdk.RemovalPolicy.DESTROY
+        )
+        access_point = fs.add_access_point(
+            "MLAccessPoint",
+            create_acl=efs.Acl(owner_gid="1001", owner_uid="1001", permissions="750"),
+            path="/export/models",
+            posix_user=efs.PosixUser(gid="1001", uid="1001"),
+        )
 
         # %%
         # iterates through the Python files in the docker directory
         docker_folder = os.path.dirname(os.path.realpath(__file__)) + "/inference"
-        pathlist = Path(docker_folder).rglob('*.py')
+        pathlist = Path(docker_folder).rglob("*.py")
         for path in pathlist:
             base = os.path.basename(path)
             filename = os.path.splitext(base)[0]
             # Lambda Function from docker image
             function = lambda_.DockerImageFunction(
-                self, filename,
-                code=lambda_.DockerImageCode.from_image_asset(docker_folder,
-                                                              cmd=[
-                                                                  filename+".handler"]
-                                                              ),
+                self,
+                filename,
+                code=lambda_.DockerImageCode.from_image_asset(
+                    docker_folder, cmd=[filename + ".handler"]
+                ),
                 memory_size=8096,
                 timeout=cdk.Duration.seconds(600),
                 vpc=vpc,
                 filesystem=lambda_.FileSystem.from_efs_access_point(
-                    access_point, '/mnt/hf_models_cache'),
-                environment={
-                    "TRANSFORMERS_CACHE": "/mnt/hf_models_cache"},
+                    access_point, "/mnt/hf_models_cache"
+                ),
+                environment={"TRANSFORMERS_CACHE": "/mnt/hf_models_cache"},
+            )
+
+            api = api_gw.RestApi(
+                self,
+                f"{filename}-api",
+                rest_api_name=f"{filename} Service",
+                description=f"This service serves {filename}.",
+                default_cors_preflight_options=api_gw.CorsOptions(
+                    allow_origins=api_gw.Cors.ALL_ORIGINS,
+                    allow_methods=api_gw.Cors.ALL_METHODS,
+                ),
             )
 
             # adds method for the function
-            lambda_integration = api_gw.LambdaIntegration(function, proxy=False, integration_responses=[
-                api_gw.IntegrationResponse(status_code='200',
-                                           response_parameters={
-                                               'method.response.header.Access-Control-Allow-Origin': "'*'"
-                                           })
-            ])
+            lambda_integration = api_gw.LambdaIntegration(function)
+
+            api.root.add_method("ANY")
+            api_resource = api.root.add_resource(filename)
+            api_resource.add_method("GET", lambda_integration)
+            api_resource.add_method("POST", lambda_integration)
+
 
 app = cdk.App()
 
--- inference/sentiment.py
+++ inference/sentiment.py
@@ -8,9 +8,16 @@ from transformers import pipeline
 
 nlp = pipeline("sentiment-analysis")
 
+
 def handler(event, context):
+    print(event)
+    input_body = json.loads(event["body"])
+    print(input_body)
+    input_text = input_body["text"]
+    sentiment_output = nlp(input_text)[0]
     response = {
         "statusCode": 200,
-        "body": nlp(event['text'])[0]
+        "headers": {"Access-Control-Allow-Origin": "*"},
+        "body": json.dumps(sentiment_output),
     }
     return response
